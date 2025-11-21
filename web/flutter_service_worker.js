32bc91a0dc338803a65ff7b74d61ec48060b1266
sa1nss <167847973+sa1nss@users.noreply.github.com>
2025-11-21 18:24:07 +0000

    fix

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "22fde775d7e4512bf38c755083a88176",
+"flutter_bootstrap.js": "0c632ee342029f867fe79c17c8ae2dae",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
